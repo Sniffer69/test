c80a729129c65bf653e875fb790f371c7ee0ce93
sniffer <karanvee8603@gmail.com>
2023-03-22 16:48:02 +0530

    Dao Created

--- Kafka_Backend/src/main/java/com/Kafka/Kafka_Backend/Dynamic_Topic/TopicRepo.java
+++ Kafka_Backend/src/main/java/com/Kafka/Kafka_Backend/Dynamic_Topic/TopicRepo.java
@@ -3,5 +3,5 @@ package com.Kafka.Kafka_Backend.Dynamic_Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface TopicRepo {
+public interface TopicRepo extends JpaRepository <TopicDao, String>{
 }
